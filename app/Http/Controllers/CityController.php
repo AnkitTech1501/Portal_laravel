b9c8d60d4e7cb2d02a20c9a7fb954620e8239754
ankitdubey91377@gmail.com <ankitdubey91377@gmail.com>
2025-03-25 07:49:28 +0530

    sdf

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -10,16 +10,16 @@ class CityController extends Controller
 {
     public function show($id)
     {
-        try{
-            $city = City::with('state')->($id);
-
+        try {
+            $city = City::with('state')->where('state_id', $id)->get();
+            return $city;
             if (!$city) {
                 return response()->json(['message' => 'City not found'], 404);
             }
-    
+
             return response()->json($city);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error fetching states', 'error' => $e->getMessage()], 500);
         }
-      
     }
-  
 }
